615f7b90f7862497cee2c3208597e5c580fdb79e
Bruno Sacurai <sacuraibruno@gmail.com>
2025-03-07 14:30:27 -0300

    tratamento de informações adquiridas da api

--- Assets/js/poke-api.js
+++ Assets/js/poke-api.js
@@ -1,6 +1,6 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 5) => {
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     return fetch(url)
     .then((response) => response.json())
@@ -13,5 +13,22 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
 pokeApi.getPokemonDetail = (pokemon) => {
    return fetch(pokemon.url)
             .then((response) => response.json())
+            .then(convertPokeApiDetailToPokemon)
 }
 
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon()
+    // nome e numero
+    pokemon.number =pokeDetail.order
+    pokemon.name = pokeDetail.name
+    // seleção do tipo principal
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+    // tipo principal e secundario
+    pokemon.types = types 
+    pokemon.type = type
+    // foto do pokemon
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
