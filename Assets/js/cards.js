d92b139b4848da08c0d6676dcd7810abcb6bd697
Bruno Sacurai <sacuraibruno@gmail.com>
2025-03-10 18:22:59 -0300

    adicionando cards dinamicos para detalhes

---
+++ Assets/js/cards.js
@@ -0,0 +1,9 @@
+function openTab(event, tabName){
+    var i, tabContent, tabLinks
+
+    tabContent = document.getElementById("tabContent")
+    for (let i = 0; i < tabLinks.length; i++) {
+       tabContent[i].style.displey = "none"
+    }
+    
+}
